9277ad600b420e4787f2c68c3210ee0b20fa1540
bingli-borland <1042996632@qq.com>
2024-09-09 21:31:46 +0800

    解析参数兼容WAS

--- java/org/apache/catalina/connector/RequestFacade.java
+++ java/org/apache/catalina/connector/RequestFacade.java
@@ -585,6 +585,21 @@ public class RequestFacade implements HttpServletRequest {
         return request.getTrailerFields();
     }
 
+    public void pushParameterStack() {
+        request.pushParameterStack();
+    }
+
+    public void aggregateQueryStringParams(String additionalQueryString, boolean setQS) {
+        request.aggregateQueryStringParams(additionalQueryString, setQS);
+    }
+
+    public void removeQSFromList() {
+        request.removeQSFromList();
+    }
+
+    public void setQueryString(String queryString) {
+        request.setQueryString(queryString);
+    }
 
     @Override
     public String getRequestId() {
