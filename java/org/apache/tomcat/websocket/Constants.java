b9c071e92903a2db5be74a7a5eed980fb40d8730
bingli-borland <1042996632@qq.com>
2026-04-08 17:40:37 +0800

    修复windows环境下getLocalAddress获取到本地地址0.0.0.0

--- java/org/apache/tomcat/websocket/Constants.java
+++ java/org/apache/tomcat/websocket/Constants.java
@@ -134,6 +134,7 @@ public class Constants {
 
     public static volatile BufferType BUFFER_TYPE = BufferType.valueOf(System.getProperty("org.apache.tomcat.websocket.bufferType", "NETTY"));
     public static int NETTY_INIT_SIZE = Integer.getInteger("org.apache.tomcat.websocket.buffer.netty.initSize", 8192);
+    public static final String CLIENT_IP_ADDRESS = System.getProperty("org.apache.tomcat.websocket.client.ip.address");
 
     private Constants() {
         // Hide default constructor
